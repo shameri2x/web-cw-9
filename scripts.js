51d7cf66433663d1cb6df1b188cac36c0a5ee16a
AhmedAlshammeri-46 <66562839+shameri2x@users.noreply.github.com>
2020-10-10 19:51:58 +0300

    done

--- scripts.js
+++ scripts.js
@@ -4,10 +4,37 @@
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
 
+
+
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+let title = document.getElementById("caption");
+let body = document.body;
 
+function change(){
+
+
+    if (title.innerHTML == "الشمس") {
+        
+        let change = document.getElementById("image");
+        change.src =  " images/moon.jpg "
+        let title = document.getElementById("caption");
+        title.innerHTML="القمر";
+        body.style.background = "#5D6D7E";
+
+    
+    } else {
+        let change = document.getElementById("image");
+        change.src =  " images/sun.jpg "
+        let title = document.getElementById("caption");
+        title.innerHTML="الشمس";
+        body.style.background = "#EC401C";
+
+    }
+  
+}
+change()
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
